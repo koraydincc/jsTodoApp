902be0c8d59c20836a3096e2b36d71ad4900842a
Koray <74928882+koraydincc@users.noreply.github.com>
2023-05-26 00:54:13 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,19 +3,43 @@ let textAdd = document.getElementById('textAdd');
 let grid = document.getElementById('grid');
 const data = [];
 
-btnAdd.addEventListener('click', add);
+document.getElementById('btnAdd').addEventListener('click',function(){
+   add();
+})
+
 
 function add() {
-  let li = document.createElement('li');
-  let removeBtn = document.createElement('button');
-  removeBtn.innerHTML = 'SİL';
-  removeBtn.className = 'removeBtn'
-  li.className = 'listText';
-  li.innerHTML = `${textAdd.value} `;
-  li.appendChild(removeBtn);
-  
-  grid.appendChild(li);
-  data.push(li);
+
+ 
+
+  if (textAdd.value == '') {
+      alert('Lütfen yapılacak bir şey ekleyin!')
+      return false
+   
+  } 
+  else{
+      let li = document.createElement('li');
+      let removeBtn = document.createElement('button');
+      removeBtn.innerHTML = 'X';
+      removeBtn.className = 'removeBtn' 
+      
+    
+      li.className = 'listText';
+      li.innerHTML = `${textAdd.value} `;
+      li.appendChild(removeBtn);
+      
+      
+      grid.appendChild(li);
+      data.push(li);
+      removeBtn.addEventListener('click',function () {
+         let index = data.indexOf(li)
+         if (index !== -1) {
+            data.splice(index,1)
+         }
+         grid.removeChild(li)
+       })
+   }
   console.log(data);
   textAdd.value = '';
 }
+
